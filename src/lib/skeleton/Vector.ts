3fc6d8c151d01c6e1bbc1d5d0b5b432f3823b735
Van Auken <DVANAUK@citgo.com>
2025-01-13 17:38:27 -0600

    Init dev

--- src/lib/skeleton/Vector.ts
+++ src/lib/skeleton/Vector.ts
@@ -49,4 +49,30 @@ export class Vector {
     clone(): Vector {
         return new Vector(this.x, this.y);
     }
+
+    static bisector(angle1: number, angle2: number): number {
+        const toRadians = Math.PI / 180;
+        const v1 = {
+            x: Math.cos(angle1 * toRadians),
+            y: Math.sin(angle1 * toRadians),
+        };
+        const v2 = {
+            x: Math.cos(angle2 * toRadians),
+            y: Math.sin(angle2 * toRadians),
+        };
+        const sumX = v1.x + v2.x;
+        const sumY = v1.y + v2.y;
+        const magnitude = Math.sqrt(sumX * sumX + sumY * sumY);
+        const bisectorVectorX = sumX / magnitude;
+        const bisectorVectorY = sumY / magnitude;
+        let bisectorAngleRadians = Math.atan2(bisectorVectorY, bisectorVectorX);
+        let bisectorAngleDegrees = bisectorAngleRadians * (180 / Math.PI);
+        
+        // Normalize angle to 0-360 degrees
+        if (bisectorAngleDegrees < 0) {
+            bisectorAngleDegrees += 360;
+        }
+        
+        return bisectorAngleDegrees;
+    }
 }
